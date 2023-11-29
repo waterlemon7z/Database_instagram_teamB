c2a6fe82bda86185fcbb940756da7c8df1c64de9
Kangai1 <144196895+Kangai1@users.noreply.github.com>
2023-11-29 10:04:02 +0900

    Update Userservice.java

--- src/entity/User/Userservice.java
+++ src/entity/User/Userservice.java
@@ -1,32 +1,66 @@
-package service;
-
 import entity.User;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserService {
-    private List<User> users = new ArrayList<>();
+    private Connection connection;
+
+    public UserService(Connection connection) {
+        this.connection = connection;
+    }
 
     // 사용자 추가
-    public void addUser(User user) {
-        users.add(user);
+    public void addUser(User user) throws SQLException {
+        String sql = "INSERT INTO users (id, user_id, pw) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, user.getId());
+            statement.setString(2, user.getUser_id());
+            statement.setString(3, user.getPw());
+            statement.executeUpdate();
+        }
     }
 
     // 사용자 조회
-    public Optional<User> getUserById(int id) {
-        return users.stream()
-                    .filter(user -> user.getId() == id)
-                    .findFirst();
+    public Optional<User> getUserById(int id) throws SQLException {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("user_id"),
+                        resultSet.getString("pw")));
+            }
+        }
+        return Optional.empty();
     }
 
     // 사용자 삭제
-    public boolean deleteUser(int id) {
-        return users.removeIf(user -> user.getId() == id);
+    public boolean deleteUser(int id) throws SQLException {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            int rowsDeleted = statement.executeUpdate();
+            return rowsDeleted > 0;
+        }
     }
 
     // 모든 사용자 조회
-    public List<User> getAllUsers() {
-        return new ArrayList<>(users);
+    public List<User> getAllUsers() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                users.add(new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("user_id"),
+                        resultSet.getString("pw")));
+            }
+        }
+        return users;
     }
 }
